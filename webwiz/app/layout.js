f42dda8e281206647ec81a875dba2df60dcec29f
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-03-30 14:37:31 +0000

    ,

--- webwiz/app/layout.js
+++ webwiz/app/layout.js
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./Header";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,7 +11,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
         {children}
       </body>
     </html>
