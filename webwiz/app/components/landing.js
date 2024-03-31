0fbf5bc8ccf346619bead4d0607f6ab7fe44cab0
Om Chillure <127024836+OmChillure@users.noreply.github.com>
2024-03-31 06:00:45 +0000

    day2

--- webwiz/app/components/landing.js
+++ webwiz/app/components/landing.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
 
-
 const landing = () => {
     return (
         <>
@@ -9,10 +8,9 @@ const landing = () => {
         <div className='w-full h-[80vh] text-white place-content-center text-center'>
             <div className='text-[110px] font-bold sans-serif' style={{ color: '#F9FB6B' }}>Web Wizard</div>
             <div className='text-xl text-white w-1/2 mx-auto'>Lörem ipsum mer ultrask hyposk, miska än tranerade. Diling monohönera pora bebinera, pack. Prelig ode, monogt serar. Hoholedes  </div>
-            {/* <div className='flex items-center justify-center'>
-              <button className='bg-black px-4 py-3 rounded-lg mt-5'>Login</button>
-              <button className=''>Logout</button>
-      </div> */}
+            <div className='flex items-center justify-center'>
+              <button className='px-4 py-3 rounded-lg mt-5 bg-[#E7A4ED] text-[#3B1C63] hover:bg-purple-300'>Get Started</button>
+      </div>
         </div>
         </>
     )
--- webwiz/app/page.js
+++ webwiz/app/page.js
@@ -1,9 +1,12 @@
+'use client'
 import Landing from "./components/landing";
+import Mid from "./components/midHome";
 
 export default function Home() {
   return (
     <main className="">
       <Landing />
+      <Mid />
     </main>
   );
 }
