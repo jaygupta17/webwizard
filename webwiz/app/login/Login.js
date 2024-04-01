7802a8410e06eb9783f6e372a091151277e1deca
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-04-01 16:31:12 +0000

    Co-authored-by: Om Chillure <OmChillure@users.noreply.github.com>

---
+++ webwiz/app/login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='h-[88vh] w-full flex justify-center items-center'>
+      <div className='w-[98%] h-[97%] flex justify-center items-center rounded-md'>
+        <div className='h-full w-1/2 bg-white/15'>
+
+        </div>
+        <div className='h-full w-1/2 bg-white/15'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- webwiz/app/page.js
+++ webwiz/app/page.js
@@ -1,12 +1,21 @@
 'use client'
 import Landing from "./components/landing";
 import Mid from "./components/midHome";
+import Login from "./login/Login";
 
-export default function Home() {
-  return (
+function Main() {
+  return(
     <main className="">
-      <Landing />
-      <Mid/>
-    </main>
+    <Landing />
+    <Mid/>
+  </main>
+  )
+}
+export default function Home() {
+  let isLogin = false;
+  return (
+    <>
+    {isLogin ? <Main/> : <Login/> }
+    </>
   );
 }
