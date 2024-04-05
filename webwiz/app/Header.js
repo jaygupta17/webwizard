759ff48b841f2ca4a73ed11c6c681a5cd967d744
OmChillure <omchillure4@gmail.com>
2024-04-05 20:02:18 +0530

    final push

--- webwiz/app/Header.js
+++ webwiz/app/Header.js
@@ -15,8 +15,8 @@ function Header() {
 
   return (
     <div className="md:h-[13vh] h-[20vh] w-full flex justify-center items-center">
-      <div className="bg-white/10 md:h-[10vh] w-[98%] rounded-[20px] flex justify-between items-center min-[200px]:h-[20vh] min-[200px]:flex-row min-[700px]:flex-row relative">
-        <div className="rounded-md ml-4">
+      <div className="bg-white/10 md:h-[10vh] w-[100%] rounded-[20px] flex justify-between items-center min-[200px]:h-[20vh] min-[200px]:flex-row min-[700px]:flex-row relative">
+        <div className="rounded-md ml-4 float-right ">
           <Image src={logo} alt="logo" width={65} height={65} />
         </div>
         <button
@@ -63,7 +63,7 @@ function Header() {
           <li className="font-semibold relative w-fit block after:block after:content-[''] after:absolute after:h-[2px] after:bg-purple-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center hover:drop-shadow-[0_0_4px_#9834c7]">
             <Link href="/">Wiz brewery</Link>
           </li>
-          <div className="md:w-[30%] h-[100%] flex justify-center items-center sm:w-full gap-2">
+          <div className="md:w-[30%] h-[100%] flex justify-center items-center sm:w-full gap-2 ml-[20%]">
           <UserButton/>
           <Link href="/support">
               <Image src={bot} alt="bot" width={50} height={50} />
--- webwiz/app/sign-up/[[...sign-up]]/page.jsx
+++ webwiz/app/sign-up/[[...sign-up]]/page.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-[90vh] w-full flex justify-center items-center">
+    <div className="h-[95vh] w-full flex justify-center items-center">
       <SignUp />
     </div>
   );
