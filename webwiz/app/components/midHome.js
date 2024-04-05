6310bedcabde37037eb03956e19f2c10c8230c22
OmChillure <omchillure4@gmail.com>
2024-04-05 18:53:07 +0530

    styling

--- webwiz/app/components/midHome.js
+++ webwiz/app/components/midHome.js
@@ -3,14 +3,14 @@ import Spline from '@splinetool/react-spline';
 export default function mid() {
     return (
         <div className='h-[150vh] max-[700px]:h-[400vh] w-[100%] flex justify-center items-center flex-col max-[700px]:gap-y-4'>
-            <div className='w-[50%] h-[70vh] flex justify-center items-center max-[700px]:h-[190vh] max-[700px]:w-[90%] max-[700px]:flex-col '>
+            <div className='w-[80%] h-[70vh] flex justify-center items-center max-[700px]:h-[190vh] max-[700px]:w-[90%] max-[700px]:flex-col ml-[15%]'>
                 <div className='w-[80%] max-[700px]:h-[90vh] max-[700px]:w-[80%]'>
                     <h2 className='text-[4vh] text-white'>The Wizards Home</h2>
                     <p className='text-white text-[2.3vh]'>The Wizard's Home wasn't your typical dwelling. Clad in living bark, it pulsed with a faint bioluminescence, casting an ethereal glow over the surrounding wildflowers. Inside, shelves groaned under the weight of grimoires and vials of shimmering potions. A crackling fire in the hearth cast dancing shadows on the walls, where fantastical creatures cavorted in silent murals. The air hummed with an intangible magic, a promise of wonder waiting to be discovered.</p>
                 </div>  
-                <Spline className='max-[700px]:w-[100%] max-[700px]:h-[90vh] w-[20%]' scene="https://prod.spline.design/xEAdY-lDDdB-dcVo/scene.splinecode" />
+                <Spline className='flex justify-center items-center max-[700px]:w-[100%] max-[700px]:h-[90vh] w-[20%]' scene="https://prod.spline.design/xEAdY-lDDdB-dcVo/scene.splinecode" />
             </div>
-            <div className='flex-row-reverse w-[50%] h-[70vh] max-[700px]:h-[190vh] max-[700px]:w-[90%] flex justify-center items-center max-[700px]:flex-col max-[700px]:w-[80%]'>
+            <div className='flex-row-reverse w-[80%] h-[70vh] max-[700px]:h-[190vh] max-[700px]:w-[90%] flex justify-center items-center max-[700px]:flex-col max-[700px]:w-[80%] mr-[15%]'>
                 <div className='w-[80%] max-[700px]:w-[80%] max-[700px]:h-[90vh]'>
                     <h2 className='text-[4vh] text-white'>The Frog Wizard</h2>
                     <p className='text-white text-[2.3vh]'>
--- webwiz/app/support/page.jsx
+++ webwiz/app/support/page.jsx
@@ -21,10 +21,10 @@ callLLm()
     <div className='md:h-[90vh] h-[80vh] w-full flex justify-center items-center '>
       <div className='w-[70%] h-[98%] flex justify-center items-center flex-col'>
       <div className='flex justify-center gap-x-2 items-center h-[7vh] w-[70%]'>
-        <input className="h-[100%] w-[60%] " type="text" value={data} onChange={e => setData(e.target.value)} />
+        <input className="h-[50%] w-[100%] " type="text" value={data} onChange={e => setData(e.target.value)} />
         <button className="w-[30%] h-[100%%] rounded-md bg-cyan-500 shadow-lg shadow-indigo-500/40 p-1 ml-6" onClick={callLLm}>Generate</button>
       </div>
-      <div className='flex justify-center items-center bg-white/10 h-[50vh] w-[70%]'>
+      <div className='flex justify-center items-center rounded-md bg-white/10 h-[50vh] w-[70%]'>
        <div className=''>
         {response}
         </div>
