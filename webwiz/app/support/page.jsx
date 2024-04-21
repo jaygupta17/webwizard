3fae212addd99c2f8e4161616ec7fc8d420feee1
Jay Gupta <146335736+jaygupta17@users.noreply.github.com>
2024-04-21 04:22:26 +0000

    bot responsive

--- webwiz/app/support/page.jsx
+++ webwiz/app/support/page.jsx
@@ -19,12 +19,12 @@ callLLm()
 }, [])
   return (
     <div className='md:h-[90vh] h-[80vh] w-full flex justify-center items-center '>
-      <div className='w-[70%] h-[98%] flex justify-center items-center flex-col'>
-      <div className='flex justify-center gap-x-2 items-center h-[7vh] w-[70%]'>
-        <input className="h-[50%] w-[100%] " type="text" value={data} onChange={e => setData(e.target.value)} />
-        <button className="w-[30%] h-[100%%] rounded-md bg-cyan-500 shadow-lg shadow-indigo-500/40 p-1 ml-6" onClick={callLLm}>Generate</button>
+      <div className='md:w-[70%] w-[90%] h-[98%] flex justify-center items-center flex-col'>
+      <div className='flex justify-center gap-x-2 items-center h-[7vh] w-[90%] md:w-[70%]'>
+        <input className="h-[70%] rounded-md bg-white/20 px-3 py-2 text-white/90 w-[70%] " type="text" value={data} onChange={e => setData(e.target.value)} />
+        <button className="w-[30%] h-[70%] text-white font-bold rounded-md bg-cyan-500 shadow-lg shadow-indigo-500/40 p-1" onClick={callLLm}>Generate</button>
       </div>
-      <div className='flex justify-center items-center rounded-md bg-white/10 h-[50vh] w-[70%]'>
+      <div className='flex justify-center items-center rounded-md bg-white/10 h-[50vh] w-[90%] md:w-[70%]'>
        <div className=''>
         {response}
         </div>
