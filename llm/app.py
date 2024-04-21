a6bdb226b104301532ecbf2358627dbf4ab4e7c2
OmChillure <omchillure4@gmail.com>
2024-04-21 09:10:19 +0530

    llm

--- llm/app.py
+++ llm/app.py
@@ -1,39 +1,35 @@
-from flask import Flask, render_template, request ,jsonify 
-from flask_cors import CORS , cross_origin
-from langchain_community.llms import HuggingFaceHub
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 import os
+from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+import google.generativeai as genai
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='./template')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-template = """Question: {question}
+load_dotenv()
+API_KEY = os.getenv("GOOGLE_API_KEY")
 
-Answer: Let's think step by step."""
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel("gemini-pro")
 
-prompt = PromptTemplate(template=template, input_variables=["question"])
-repo_id = "google/flan-t5-xxl"
-llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 90}
-)
+def generate_response(question):
+    response = model.generate_content(question)
+    return response.text
 
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+@app.route("/")
+@cross_origin()
+def index():
+    return render_template("index.html")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/get-response')
+@app.route("/get-response")
 @cross_origin()
 def get_response():
-    print(request.form)
-    user_input = request.args.get('user_input')
-    response = llm_chain.run(user_input)
+    question = request.args["question"]
+    response = generate_response(question)
     return response
 
-if __name__ == '__main__':
-    app.run(debug=True , port='5000')
+if __name__ == "__main__":
+    app.run(debug=True)
